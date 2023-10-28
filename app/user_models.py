25f333c6041db92c3dc6392f49ddc38a5f313ece
Abiel Sanchez <abiel02sz@gmail.com>
2023-10-27 23:33:32 -0500

    cositas

--- app/user_models.py
+++ app/user_models.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+import uuid
 from sqlalchemy.orm import relationship
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    email = Column(String(255), unique=True, nullable=True)
+    password = Column(String(100))
     is_admin = Column(Boolean, default=False)
