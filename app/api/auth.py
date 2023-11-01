3ecfde1d4bc0ef7bc40db1d55f85512bdb9dd997
Abiel Sanchez <abiel02sz@gmail.com>
2023-11-01 02:46:54 -0500

    auth arreglos

--- app/api/auth.py
+++ app/api/auth.py
@@ -6,32 +6,45 @@ import bcrypt
 from ..models.user_models import User
 from ..database.database import SessionLocal
 
+
 class UserLogin(BaseModel):
     name: str
     password: str
 
+
 auth_router = APIRouter()
+
 
 @auth_router.post("/login")
 def login(user: UserLogin):
     try:
-        # todo: incriptar la contraseña entrante
+        # Crear una instancia de la sesión de la base de datos
         db = SessionLocal()
-        user_in_db = db.query(User).filter(User.name == user.name).first()
-          
-        # Verifica si el usuario existe
-        if not user_in_db:  
-            return JSONResponse(content={"message": "Usuario no encontrado"}, status_code=404)
-        
-        # Verifica la contraseña ingresada por el usuario con la contraseña almacenada
-        if not bcrypt.checkpw(user.password, user_in_db.password):
-            return JSONResponse(content={"message": "Contraseña incorrecta"}, status_code=400)
 
-        
-        return JSONResponse(content={"message": "Inicio de sesión exitoso"}, status_code=200)
+        # Buscar el usuario por nombre en la base de datos
+        user_in_db = db.query(User).filter(User.name == user.name).first()
+
+        # Verificar si el usuario existe
+        if not user_in_db:
+            return JSONResponse(
+                content={"message": "Usuario no encontrado"}, status_code=404
+            )
+
+        # Verificar la contraseña ingresada por el usuario con la contraseña almacenada
+        if bcrypt.checkpw(user.password.encode("utf-8"), user_in_db.password):
+            return JSONResponse(
+                content={"message": "Inicio de sesión exitoso"}, status_code=200
+            )
+        else:
+            return JSONResponse(
+                content={"message": "Contraseña incorrecta"}, status_code=400
+            )
+
     except Exception as e:
         # Manejar la excepción de manera apropiada, por ejemplo, registrando el error
         print("error: " + str(e))
-        return JSONResponse(content={"message": "Error interno del servidor"}, status_code=500)
+        return JSONResponse(
+            content={"message": "Error interno del servidor"}, status_code=500
+        )
     finally:
-        db.close()  
+        db.close()
--- app/api/user.py
+++ app/api/user.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 import bcrypt
-from ..database.database  import engine, SessionLocal, Base
+from ..database.database import engine, SessionLocal, Base
 from ..models.user_models import User
 from ..schemas.user_schema import UserScheme
 
 user_router = APIRouter()
+
 
 @user_router.get("/getUsers")
 def read_users():
@@ -14,13 +15,18 @@ def read_users():
     db.close()
     return Users
 
+
 @user_router.post("/postUsers")
 def create_user(user: UserScheme):
-    user.password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt(8))
+    user.password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt(8))
     db = SessionLocal()
-    db_user = User(name=user.name, email=user.email, password=user.password, is_admin=False)
-    
+    db_user = User(
+        name=user.name, email=user.email, password=user.password, is_admin=False
+    )
+
     db.add(db_user)
     db.commit()
     db.close()
-    return JSONResponse(content={"message" : "usuario creado exitosamente."} , status_code=201)
+    return JSONResponse(
+        content={"message": "usuario creado exitosamente."}, status_code=201
+    )
